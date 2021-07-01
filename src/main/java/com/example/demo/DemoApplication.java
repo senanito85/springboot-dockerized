d019b8cf8fb12109e143c034dd1eb5b7d29f4abe
senanito85 <sysad85@gmail.com>
2021-07-01 22:23:28 +1000

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+	@GetMapping("/bye")
+	public String bye(@RequestParam(value = "name", defaultValue = "User") String name) {
+		return String.format("Good Bye %s!", name);
+	}
+
+}
+
+@Configuration
+class StaticConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/").addResourceLocations("classpath:/static/");
+	}
+}
